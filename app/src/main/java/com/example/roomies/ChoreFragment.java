4cd84df8efda6d292b0698e0a9179b4b92a5d1c4
srnazng <serenazeng@fb.com>
2022-06-24 11:18:24 -0700

    View list of chores belonging to circle

--- app/src/main/java/com/example/roomies/ChoreFragment.java
+++ app/src/main/java/com/example/roomies/ChoreFragment.java
@@ -1,15 +1,31 @@
 package com.example.roomies;
 
+import static com.example.roomies.HomeFragment.currentCircle;
+
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.roomies.adapter.ChoreAdapter;
+import com.example.roomies.model.Chore;
+import com.example.roomies.model.UserCircle;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Fragment for chore list and related actions
@@ -18,6 +34,12 @@ public class ChoreFragment extends Fragment {
 
     private Button btnToCalendar;
     private Button btnAddChore;
+    private RecyclerView rvChores;
+    private ChoreAdapter adapter;
+
+    public List<Chore> choreList;
+
+    public static final String TAG = "ChoreFragment";
 
     public ChoreFragment() {
         // Required empty public constructor
@@ -44,6 +66,18 @@ public class ChoreFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_chore, container, false);
 
+        // Bind to recycler view
+        rvChores = view.findViewById(R.id.rvChores);
+        // Initialize chores
+        choreList = new ArrayList<>();
+        updateChoreList();
+        // Create adapter passing in the chore data
+        adapter = new ChoreAdapter(choreList);
+        // Attach the adapter to the recyclerview to populate items
+        rvChores.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvChores.setLayoutManager(new LinearLayoutManager(getActivity()));
+
         // button to calendar fragment
         btnToCalendar = view.findViewById(R.id.btnToCalendar);
         btnToCalendar.setOnClickListener(new View.OnClickListener() {
@@ -65,11 +99,48 @@ public class ChoreFragment extends Fragment {
         return view;
     }
 
+    // update chore list whenever screen resumes
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateChoreList();
+    }
+
     // go to calendar page
     public void toCalendar() {
         FragmentTransaction fragmentTransaction = (getActivity()).getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.frame, CalendarFragment.newInstance());
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+    }
+
+    // query database for circle's list of chores
+    public void updateChoreList(){
+        // only get chores for user's current circle
+        ParseQuery<Chore> query = ParseQuery.getQuery(Chore.class).whereEqualTo(Chore.KEY_CIRCLE, currentCircle);
+        // include data referred by user key
+        query.include(UserCircle.KEY_USER);
+        // start an asynchronous call for Chore objects
+        query.findInBackground(new FindCallback<Chore>() {
+            @Override
+            public void done(List<Chore> chores, ParseException e) {
+                // check for errors
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting userCircles", e);
+                    Toast.makeText(getActivity(), "Unable to retrieve chores", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // no chores
+                if(chores.isEmpty()){
+                    Toast.makeText(getActivity(), "No chores today!", Toast.LENGTH_SHORT).show();
+                }
+
+                // save received chores to list and notify adapter of new data
+                choreList.clear();
+                choreList.addAll(chores);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/roomies/model/Chore.java
+++ app/src/main/java/com/example/roomies/model/Chore.java
@@ -2,8 +2,60 @@ package com.example.roomies.model;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.Date;
 
 @ParseClassName("Chore")
 public class Chore extends ParseObject {
-    // TODO: add setter and getters
+    public static final String KEY_CREATOR = "creator";
+    public static final String KEY_CIRCLE = "circle";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_PRIORITY = "priority";
+    public static final String KEY_POINTS = "points";
+    public static final String KEY_ALL_DAY = "allDay";
+    public static final String KEY_DUE = "dueDatetime";
+
+    public ParseUser getCreator(){
+        return getParseUser(KEY_CREATOR);
+    }
+
+    public void setCreator(ParseUser user){
+        put(KEY_CREATOR, user);
+    }
+
+    public Circle getCircle(){
+        return (Circle) getParseObject(KEY_CIRCLE);
+    }
+
+    public void setCircle(Circle circle) { put(KEY_CIRCLE, circle); }
+
+    public String getTitle() { return getString(KEY_TITLE); }
+
+    public void setTitle(String title) { put(KEY_TITLE, title); }
+
+    public String getDescription() { return getString(KEY_DESCRIPTION); }
+
+    public void setDescription(String description) {put(KEY_DESCRIPTION, description); }
+
+    public String getPriority() { return getString(KEY_PRIORITY); }
+
+    public void setPriorityLow() { put(KEY_TITLE, "Low"); }
+
+    public void setPriorityMed() { put(KEY_TITLE, "Medium"); }
+
+    public void setPriorityHigh() { put(KEY_TITLE, "High"); }
+
+    public int getPoints() { return getInt(KEY_POINTS); }
+
+    public void setPoints(int points) { put(KEY_POINTS, points); }
+
+    public Boolean getAllDay() { return getBoolean(KEY_ALL_DAY); }
+
+    public void setAllDay(Boolean allDay) { put(KEY_ALL_DAY, allDay); }
+
+    public Date getDue() { return getDate(KEY_DUE); }
+
+    public void setDue(Date date) { put(KEY_DUE, date); }
 }
